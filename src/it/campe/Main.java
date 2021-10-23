b884b52ec5f925dfa1daa488038e1dda94827298
JanaZhakPl <janazhakpl@gmail.com>
2021-10-23 15:09:12 +0200

    Task complete.

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int six = 6;
-        ++six; ++six; ++six;
-        //six += 3;
+        six += 3;
 
         System.out.println(six);
     }
